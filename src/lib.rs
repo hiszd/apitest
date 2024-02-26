eaf0edc93dd74506b9929771f6123faf3384b7f0
Zion Koyl <hiszd1@gmail.com>
2024-02-26 16:58:02 -0500

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
+use std::env;
+
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenvy::dotenv;
-use rocket::http::Header;
-use rocket::Response;
-use std::env;
+use uuid::Uuid;
 
 extern crate tera;
 #[macro_use]
@@ -16,59 +16,109 @@ pub mod routes;
 pub mod schema;
 pub mod types;
 
-pub struct MyConfig {
-    users_update: bool,
-    tickets_update: bool,
-    subscribers: Vec<Box<dyn FnOnce()>>,
+#[derive(PartialEq, Debug, Clone)]
+pub enum Topic {
+  Users,
+  Tickets,
 }
 
-impl MyConfig {
-    pub fn new() -> MyConfig {
-        MyConfig {
-            users_update: false,
-            tickets_update: false,
-            subscribers: Vec::new(),
-        }
-    }
-    pub const fn const_new() -> MyConfig {
-        MyConfig {
-            users_update: false,
-            tickets_update: false,
-            subscribers: Vec::new(),
-        }
-    }
-    pub fn subscribe(&mut self, func: Box<dyn FnOnce()>) {
-        self.subscribers.push(func);
-    }
-    pub fn get_users_update(&self) -> bool {
-        self.users_update
-    }
-    pub fn set_users_update(&mut self, value: bool) {
-        self.users_update = value;
-    }
-    pub fn get_tickets_update(&self) -> bool {
-        self.tickets_update
-    }
-    pub fn set_tickets_update(&mut self, value: bool) {
-        self.tickets_update = value;
-    }
+// impl From<String> for Topic {
+//   fn from(s: String) -> Self {
+//     match s {
+//       String::from("users") => Topic::Users,
+//       String::from("Users") => Topic::Users,
+//       String::from("tickets") => Topic::Tickets,
+//       String::from("Tickets") => Topic::Tickets,
+//       _ => Topic::Users,
+//     }
+//   }
+// }
+
+// impl From<&str> for Topic {
+//   fn from(s: &str) -> Self {
+//     match s {
+//       "users" => Topic::Users,
+//       "Users" => Topic::Users,
+//       "tickets" => Topic::Tickets,
+//       "Tickets" => Topic::Tickets,
+//       _ => Topic::Users,
+//     }
+//   }
+// }
+
+pub struct Subscriber {
+  pub name: String,
+  pub id: String,
+  pub topic: Topic,
+  pub needs_update: bool,
 }
 
-pub fn add_headers<'r>(response: &'r mut Response<'r>) -> &'r mut rocket::Response<'r> {
-    response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-    response.set_header(Header::new(
-        "Access-Control-Allow-Methods",
-        "POST, GET, OPTIONS",
-    ));
-    response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-    response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    return response;
+impl Subscriber {
+  pub fn new(name: &str, topic: Topic) -> Subscriber {
+    Subscriber {
+      name: name.to_string(),
+      id: Uuid::new_v4().to_string(),
+      needs_update: false,
+      topic,
+    }
+  }
+}
+
+pub struct CustState {
+  users_update: bool,
+  tickets_update: bool,
+  subscribers: Vec<Subscriber>,
+}
+
+impl CustState {
+  pub fn new() -> CustState {
+    CustState {
+      users_update: false,
+      tickets_update: false,
+      subscribers: Vec::new(),
+    }
+  }
+  pub const fn const_new() -> CustState {
+    CustState {
+      users_update: false,
+      tickets_update: false,
+      subscribers: Vec::new(),
+    }
+  }
+  pub fn subscribe(&mut self, name: &str, topic: Topic) -> &str {
+    let sub = Subscriber::new(name, topic);
+    self.subscribers.push(sub);
+    sub.id.as_str()
+  }
+  pub fn unsubscribe(&mut self, id: &str) {
+    self.subscribers.retain(|s| s.id != id);
+  }
+  pub fn trigger_update(&mut self, topic: Topic) {
+    self.subscribers.iter_mut().map(|s| {
+      if s.topic == topic {
+        s.needs_update = true
+      }
+    });
+  }
+  pub fn check_update(&mut self, id: &str) -> bool {
+    let mut rtrn = false;
+    self
+      .subscribers
+      .iter_mut()
+      .find(|s| s.id == id)
+      .map(|s| {
+        s.needs_update = false;
+        rtrn = s.needs_update
+      })
+      .unwrap();
+    rtrn
+  }
 }
 
 pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
+  dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {database_url}"))
+  let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+  PgConnection::establish(&database_url)
+    .unwrap_or_else(|_| panic!("Error connecting to {database_url}"))
 }
--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,240 +1,241 @@
 // use rocket::futures::FutureExt;
-use rocket::tokio::sync::Mutex;
-
 use diesel::prelude::*;
+#[allow(unused_imports)]
+use rocket::futures::{SinkExt, StreamExt};
 use rocket::response::status::NoContent;
+use rocket::serde::json::Json;
+use rocket::tokio::sync::Mutex;
 
 use crate::establish_connection;
 use crate::model::*;
 use crate::schema::*;
 use crate::types::json::shared::WithSecret;
 use crate::types::json::user::*;
-use crate::MyConfig;
-#[allow(unused_imports)]
-use rocket::futures::{SinkExt, StreamExt};
-use rocket::serde::json::Json;
+use crate::CustState;
+use crate::Topic;
 
-static STATE: Mutex<MyConfig> = Mutex::const_new(MyConfig::const_new());
+static STATE: Mutex<CustState> = Mutex::const_new(CustState::const_new());
 
 fn delete_user(user_id: i32, conn: &mut PgConnection) -> Result<Json<User>, diesel::result::Error> {
-    println!("Deleting user {}", user_id);
-    let usr = users::table
-        .filter(users::id.eq(user_id))
-        .select(User::as_select())
-        .get_result(conn);
-    let tiks = tickets_authors::table
-        .inner_join(tickets::table)
-        .filter(tickets_authors::author_id.eq(user_id))
-        .select(Ticket::as_select())
-        .load(&mut establish_connection())
-        .unwrap();
-    tiks.iter().for_each(|tik| {
-        diesel::delete(tickets_authors::table)
-            .filter(tickets_authors::ticket_id.eq(tik.id))
-            .execute(conn)
-            .unwrap();
-        diesel::delete(tickets::table)
-            .filter(tickets::id.eq(tik.id))
-            .execute(conn)
-            .unwrap();
-    });
-    let del_auth_rel = diesel::delete(tickets_authors::table)
-        .filter(tickets_authors::author_id.eq(user_id))
-        .execute(conn);
-    let del_user = diesel::delete(users::table)
-        .filter(users::id.eq(user_id))
-        .execute(conn);
+  println!("Deleting user {}", user_id);
+  let usr = users::table
+    .filter(users::id.eq(user_id))
+    .select(User::as_select())
+    .get_result(conn);
+  let tiks = tickets_authors::table
+    .inner_join(tickets::table)
+    .filter(tickets_authors::author_id.eq(user_id))
+    .select(Ticket::as_select())
+    .load(&mut establish_connection())
+    .unwrap();
+  tiks.iter().for_each(|tik| {
+    diesel::delete(tickets_authors::table)
+      .filter(tickets_authors::ticket_id.eq(tik.id))
+      .execute(conn)
+      .unwrap();
+    diesel::delete(tickets::table)
+      .filter(tickets::id.eq(tik.id))
+      .execute(conn)
+      .unwrap();
+  });
+  let del_auth_rel = diesel::delete(tickets_authors::table)
+    .filter(tickets_authors::author_id.eq(user_id))
+    .execute(conn);
+  let del_user = diesel::delete(users::table)
+    .filter(users::id.eq(user_id))
+    .execute(conn);
 
-    if usr.is_err() {
-        println!("User not found");
-        Err(usr.err().unwrap())
-    } else if del_auth_rel.is_err() {
-        println!("User relation not removed");
-        Err(del_auth_rel.err().unwrap())
-    } else if del_user.is_err() {
-        println!("User not removed");
-        Err(del_user.err().unwrap())
-    } else {
-        Ok(Json(usr.unwrap()))
-    }
+  if usr.is_err() {
+    println!("User not found");
+    Err(usr.err().unwrap())
+  } else if del_auth_rel.is_err() {
+    println!("User relation not removed");
+    Err(del_auth_rel.err().unwrap())
+  } else if del_user.is_err() {
+    println!("User not removed");
+    Err(del_user.err().unwrap())
+  } else {
+    Ok(Json(usr.unwrap()))
+  }
 }
 #[get("/user/new/test")]
 pub fn new_user_test() -> Json<User> {
-    let newuser = NewUser {
-        name: "test".to_string(),
-        email: "test@test.com".to_string(),
-    };
-    let usr = diesel::insert_into(users::table)
-        .values(&newuser)
-        .returning(User::as_returning())
-        .get_result(&mut establish_connection());
-    Json(usr.unwrap())
+  let newuser = NewUser {
+    name: "test".to_string(),
+    email: "test@test.com".to_string(),
+  };
+  let usr = diesel::insert_into(users::table)
+    .values(&newuser)
+    .returning(User::as_returning())
+    .get_result(&mut establish_connection());
+  Json(usr.unwrap())
 }
 
 #[options("/user/new")]
 pub fn new_user_preflight() -> NoContent {
-    NoContent
+  NoContent
 }
 
 #[post("/user/new", data = "<user>")]
 pub fn new_user(user: Json<WithSecret<NewUserJson>>) -> Json<User> {
-    assert!(
-        user.secret == crate::SECRET,
-        "Wrong secret: {}, {}",
-        user.secret,
-        crate::SECRET
-    );
+  assert!(
+    user.secret == crate::SECRET,
+    "Wrong secret: {}, {}",
+    user.secret,
+    crate::SECRET
+  );
 
-    let newuser = NewUser {
-        name: user.data.name.clone(),
-        email: user.data.email.clone(),
-    };
-    let usr = diesel::insert_into(users::table)
-        .values(&newuser)
-        .returning(User::as_returning())
-        .get_result(&mut establish_connection());
-    Json(usr.unwrap())
+  let newuser = NewUser {
+    name: user.data.name.clone(),
+    email: user.data.email.clone(),
+  };
+  let usr = diesel::insert_into(users::table)
+    .values(&newuser)
+    .returning(User::as_returning())
+    .get_result(&mut establish_connection());
+  Json(usr.unwrap())
 }
 
 #[options("/user/update")]
 pub fn update_user_preflight() -> NoContent {
-    NoContent
+  NoContent
 }
 
 #[post("/user/update", data = "<data>")]
 pub fn update_user(data: Json<WithSecret<UserJson>>) -> Result<Json<User>, ()> {
-    if data.secret != crate::SECRET {
-        println!("Wrong secret: {}, {}", data.secret, crate::SECRET);
-        return Err(());
+  if data.secret != crate::SECRET {
+    println!("Wrong secret: {}, {}", data.secret, crate::SECRET);
+    return Err(());
+  }
+
+  println!("Updating user {:?}", data);
+
+  let rslt = diesel::update(users::table.filter(users::id.eq(data.data.id)))
+    .set((
+      users::name.eq(data.data.name.clone()),
+      users::email.eq(data.data.email.clone()),
+    ))
+    .get_result(&mut establish_connection());
+
+  match rslt {
+    Ok(usr) => {
+      println!("Updated user {:?}", usr);
+      Ok(Json(usr))
     }
-
-    println!("Updating user {:?}", data);
-
-    let rslt = diesel::update(users::table.filter(users::id.eq(data.data.id)))
-        .set((
-            users::name.eq(data.data.name.clone()),
-            users::email.eq(data.data.email.clone()),
-        ))
-        .get_result(&mut establish_connection());
-
-    match rslt {
-        Ok(usr) => {
-            println!("Updated user {:?}", usr);
-            Ok(Json(usr))
-        }
-        Err(_) => Err(()),
-    }
+    Err(_) => Err(()),
+  }
 }
 
 #[post("/user/get", data = "<data>")]
 pub fn get_user(data: Json<WithSecret<UserSelectJson>>) -> Result<Json<User>, ()> {
-    if data.secret != crate::SECRET {
-        println!("Wrong secret: {}, {}", data.secret, crate::SECRET);
-        return Err(());
-    }
-    let mut fltr = users::table.into_boxed();
-    if let Some(id) = data.data.id {
-        fltr = fltr.filter(users::id.eq(id));
-    }
-    if let Some(name) = &data.data.name {
-        fltr = fltr.filter(users::name.eq(name));
-    }
-    if let Some(email) = &data.data.email {
-        fltr = fltr.filter(users::email.eq(email));
-    }
-    Ok(Json(
-        fltr.first(&mut establish_connection())
-            .expect("Error loading users"),
-    ))
+  if data.secret != crate::SECRET {
+    println!("Wrong secret: {}, {}", data.secret, crate::SECRET);
+    return Err(());
+  }
+  let mut fltr = users::table.into_boxed();
+  if let Some(id) = data.data.id {
+    fltr = fltr.filter(users::id.eq(id));
+  }
+  if let Some(name) = &data.data.name {
+    fltr = fltr.filter(users::name.eq(name));
+  }
+  if let Some(email) = &data.data.email {
+    fltr = fltr.filter(users::email.eq(email));
+  }
+  Ok(Json(
+    fltr
+      .first(&mut establish_connection())
+      .expect("Error loading users"),
+  ))
 }
 
 #[options("/user/get")]
 pub fn get_user_preflight() -> NoContent {
-    NoContent
+  NoContent
 }
 
 // TODO: change all requests to POST and confirm secret before doing anything
 #[get("/user/list/all")]
 pub fn list_users() -> Json<Vec<User>> {
-    Json(
-        users::table
-            .select(User::as_select())
-            .load(&mut establish_connection())
-            .unwrap(),
-    )
+  Json(
+    users::table
+      .select(User::as_select())
+      .load(&mut establish_connection())
+      .unwrap(),
+  )
 }
 
 #[options("/user/list/all")]
 pub fn list_users_preflight() -> NoContent {
-    NoContent
+  NoContent
 }
 
 #[post("/user/remove", data = "<data>")]
 pub fn remove_user(data: Json<WithSecret<UserSelectJson>>) -> Result<Json<User>, ()> {
-    println!("{:?}", data);
+  println!("{:?}", data);
 
-    let conn = &mut establish_connection();
+  let conn = &mut establish_connection();
 
-    if data.secret != crate::SECRET {
-        println!("Wrong secret: {}, {}", data.secret, crate::SECRET);
-        return Err(());
+  if data.secret != crate::SECRET {
+    println!("Wrong secret: {}, {}", data.secret, crate::SECRET);
+    return Err(());
+  }
+
+  let mut fltr = users::table.into_boxed();
+
+  if let Some(id) = data.data.id {
+    fltr = fltr.filter(users::id.eq(id));
+  }
+  if let Some(name) = &data.data.name {
+    fltr = fltr.filter(users::name.eq(name));
+  }
+  if let Some(email) = &data.data.email {
+    fltr = fltr.filter(users::email.eq(email));
+  }
+
+  let user: User = fltr.first(conn).expect("Error loading users");
+
+  match delete_user(user.id, conn) {
+    Ok(usr) => Ok(usr),
+    Err(e) => {
+      println!("Error: {}", e);
+      Err(())
     }
-
-    let mut fltr = users::table.into_boxed();
-
-    if let Some(id) = data.data.id {
-        fltr = fltr.filter(users::id.eq(id));
-    }
-    if let Some(name) = &data.data.name {
-        fltr = fltr.filter(users::name.eq(name));
-    }
-    if let Some(email) = &data.data.email {
-        fltr = fltr.filter(users::email.eq(email));
-    }
-
-    let user: User = fltr.first(conn).expect("Error loading users");
-
-    match delete_user(user.id, conn) {
-        Ok(usr) => Ok(usr),
-        Err(e) => {
-            println!("Error: {}", e);
-            Err(())
-        }
-    }
+  }
 }
 
 #[options("/user/remove")]
 pub fn remove_user_preflight() -> NoContent {
-    NoContent
+  NoContent
 }
 
 #[get("/user/reset")]
 pub async fn reset_users() -> NoContent {
-    println!("Reset users start");
-    STATE.lock().await.set_users_update(true);
-    println!("Reset users");
-    NoContent
+  println!("Reset users start");
+  STATE.lock().await.trigger_update(Topic::Users);
+  println!("Reset users");
+  NoContent
 }
 
 #[get("/user/stream")]
 pub fn user_stream<'r>(ws: rocket_ws::WebSocket) -> rocket_ws::Channel<'r> {
-    ws.channel(move |mut stream| {
-        Box::pin(async move {
-            let _ = stream
-                .send(rocket_ws::Message::text("hello".to_string()))
-                .await;
-            loop {
-                let update_users = STATE.lock().await.get_users_update();
-                if update_users {
-                    println!("Sending update");
-                    stream
-                        .send(rocket_ws::Message::text("refresh".to_string()))
-                        .await
-                        .unwrap();
-                    STATE.lock().await.set_users_update(false);
-                }
-                rocket::tokio::time::sleep(std::time::Duration::from_millis(500)).await;
-            }
-        })
+  ws.channel(move |mut stream| {
+    Box::pin(async move {
+      let id = STATE.lock().await.subscribe("stream", Topic::Users);
+      let _ = stream
+        .send(rocket_ws::Message::text("hello".to_string()))
+        .await;
+      loop {
+        let update_users = STATE.lock().await.check_update(id);
+        if update_users {
+          println!("Sending update");
+          stream
+            .send(rocket_ws::Message::text("refresh".to_string()))
+            .await
+            .unwrap();
+        }
+        rocket::tokio::time::sleep(std::time::Duration::from_millis(500)).await;
+      }
     })
+  })
 }
